024345b63f2e5a06af7743ecea55432b43632a1b
Lilith Schmidl <lilith.schmidl@study.hs-duesseldorf.de>
2026-03-11 20:39:29 +0100

    metrics/hamming: implement vectorized hamming distance for pHash vectors

--- src/image_recommender/metrics/hamming.py
+++ src/image_recommender/metrics/hamming.py
@@ -15,3 +15,25 @@ def hamming_distance(query: np.ndarray, candidate: np.ndarray) -> int:
     distance = int(np.count_nonzero(bit_difference))
 
     return distance
+
+
+def hamming_distance_to_many(query: np.ndarray, candidates: np.ndarray) -> np.ndarray:
+    # ensure input dimensions are correct
+    if query.ndim != 1 or candidates.ndim != 2:
+        raise ValueError(
+            f"Dimension of query: {query.ndim} or candidate: {candidates.ndim} are wrong. Expected query: 1, candidates: 2 "
+        )
+
+    # ensure input lengths match
+    if query.shape[0] != candidates.shape[1]:
+        raise ValueError(
+            f"Length of query: {query.shape} and candidate vectors: {candidates.shape} doesn't match"
+        )
+
+    # compare query vector to each candidate row
+    bit_difference = query != candidates
+
+    # compute hamming distances
+    distances = np.count_nonzero(bit_difference, axis=1)
+
+    return distances
