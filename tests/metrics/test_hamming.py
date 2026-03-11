67830f9f5e3815a5610da07d5949f7a9b94f6ce8
Lilith Schmidl <lilith.schmidl@study.hs-duesseldorf.de>
2026-03-11 20:51:44 +0100

    tests/test_hamming: add symmetry test for scalar hamming distance

--- tests/metrics/test_hamming.py
+++ tests/metrics/test_hamming.py
@@ -8,3 +8,13 @@ def test_self_distance():
     result = hamming_distance(query=vector, candidate=vector)
 
     assert result == 0
+
+
+def test_symmetry():
+    vector_1 = np.array([1, 0, 1, 0, 1, 0, 1, 0], dtype=np.uint8)
+    vector_2 = np.array([1, 1, 1, 1, 1, 1, 1, 1], dtype=np.uint8)
+
+    result_1 = hamming_distance(query=vector_1, candidate=vector_2)
+    result_2 = hamming_distance(query=vector_2, candidate=vector_1)
+
+    assert result_1 == result_2
