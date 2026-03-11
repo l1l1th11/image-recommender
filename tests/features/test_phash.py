5010a3511a9751a9859d0fdb9fad7e1bd1d7a042
Lilith Schmidl <lilith.schmidl@study.hs-duesseldorf.de>
2026-03-11 11:14:41 +0100

    tests/test_phash: check function is deterministic

--- tests/features/test_phash.py
+++ tests/features/test_phash.py
@@ -13,3 +13,12 @@ def test_output():
     assert binary_vector.shape == (PHASH_DIM,)
     assert binary_vector.dtype == np.uint8
     assert np.all((binary_vector == 0) | (binary_vector == 1))
+
+
+def test_determinism():
+    img = load_rgb("data/samples/image_1306.jpg")
+    binary_vector_1 = extract_phash(img)
+    binary_vector_2 = extract_phash(img)
+
+    # check same image produces identical hashes
+    assert np.array_equal(binary_vector_1, binary_vector_2)
